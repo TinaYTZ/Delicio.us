a8821fb4c7d0430f75b9e5ed3acbe4a402548533
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-10-24 16:17:27 -0700

    clean the code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,13 @@
+/* jslint maxlen: 500 */
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
     'use strict';
-
-
-  var socket= io.connect();
   
-  var $userFormArea=$("#userFormArea");
-  var $userForm=$("#userForm");
-  var $users=$('#users');
+  
+  var socket= io.connect();  
+  var $userForm=$('#userForm');
   var $username=$('#username');
-  var $mainArea=$("#mainArea");
-  var $uploadForm=$('#uploadForm');
-  var $upload=$('#upload');
+  var $mainArea=$('#mainArea');
   var $userList=$('#userList');
   var $pictureArea=$('#pictureArea') ;
   var $indexbanner=$('#index-banner');
@@ -19,7 +16,7 @@ var main = function () {
   socket.on('get users', function(data){
     var html='';
     for (var i = 0; i < data.length; i++) {
-   // html+='<li  class="bg-info" >'+ data[i] +'</li>';
+   // html+='<li  class='bg-info' >'+ data[i] +'</li>';
    html+='<p class="btn-info btn-sm"> <span class="glyphicon glyphicon-user"></span>' + data[i]+ '</p>';
   }
   $userList.html(html);
@@ -27,14 +24,12 @@ var main = function () {
 
 
   socket.on('images', function(data){
-    console.log(data); 
-    console.log(data[0]['path']);
+    //console.log(data); 
+    //console.log(data[0]['path']);
     var html='';
-  
-    //console.log(array[]["path"]);
-
+    //console.log(array[]['path']);
     for (var i = 0; i < data.length; i++) {
-      html+='<img class="img-responsive col-md-4" id="kfood' + i + '" src="'+data[i]['path']+'" alt="">';
+      html+='<img class="img-responsive col-md-4" id="kfood" src="'+data[i].path+'"alt="" >';
     }
     $pictureArea.html(html);
 
@@ -58,184 +53,6 @@ var main = function () {
       });
       $username.val('');
   });
-}
+};
+
 $(document).ready(main);
-
-
-
-
-
-
-//try to use ajax handle uploading
-// $uploadForm.submit(function(e) {
-
-//    e.preventDefault(e);
-
-//    var data = new FormData(this);
-//    var file = $("#upload")[0].files[0];
-//    console.log(file);
-   
-//    var filedata = {
-//     file: {
-//       name: file.name,
-//       modified: file.lastModified,
-//       modifiedHuman: file.lastModifiedDate,
-//       size: file.size,
-//       type: file.type 
-//     }
-//    };
-
-//    var fileobj = JSON.stringify(filedata);
-//    console.log(fileobj);
-//    data.append("files", file);
-//    data.append("title", "UploadImg");
-//    console.log(data);
-
-//     $.ajax({ 
-//             type: "POST",
-//             url: "/upload",
-//             timeout:2000,
-//             dataType: "json",
-//             data: fileobj,
-//             contentType: false,
-//         success: function(response) {
-//             console.log('Success: ' + response);
-//         }//,
-//         //error: function(xhr) {
-//         //    console.log('Error: ' + xhr.status);
-//         //}
-//     });
-
-// });
-
-  /*
-  $uploadForm.submit(file,function(e){
-      e.preventDefault();
-      var files = $('#upload').val();
-      handlePOST('/upload', '.uploadresult', file);
-      $upload.val('');
-  });
-
-      // Send POST request via ajax
-    function handlePOST(url, obj, json) {
-        $.ajax({
-            type: 'POST',
-            url: url,
-            timeout: 15000,
-            data: json,
-            success: function(data) {
-                // Get result data
-                console.log('Result success');
-                $(obj).text("Upload successfully");
-            },
-            error: function (result) {
-                console.log('ajax error ' + result.status);
-            }
-        });
-    }
-
-
-
-};
-
-
-*/
-
-/*var main = function () {
-    'use strict';
-     var socket = io.connect();
-
-
-    var $mainArea=$("#mainArea");
-    var $userFormArea=$("#userFormArea");
-    var $userForm=$("#userForm");
-    var $username=$('#username');
-
-
-
-    
-    socket.on('get users', function(data){
-    var html='';
-    for (var i = 0; i < data.length; i++) {
-    html+='<li class="list-group-item">'+ data[i] +'</li>'
-    }
-    $users.html(html);
-    });
-
-    $userForm.submit(function(e){
-    e.preventDefault();
-    socket.emit('new user', $username.val(),function(data){
-    if(data){
-      $userFormArea.hide();
-      $mainArea.show();
-        }
-    });
-      $username.val('');
-});
-
-
-
- // Send POST request via ajax
-    function handlePOST(url, obj, json) {
-        $.ajax({
-            type: 'POST',
-            url: url,
-            timeout: 15000,
-            data: json,
-            success: function(data) {
-                // Get result data
-                console.log('Result' + data);
-                $(obj).text(data);
-            },
-            error: function (result) {
-                console.log('ajax error ' + result.status);
-            }
-        });
-    }
-
- 
-
- /*function 
- handleGET(url) {
-  $.ajax({
-  	type:'GET',
-  	url:url,
-  	data:json,
-  	error: function() {
-         $('#info').html('<p>An error has occurred</p>');
-      },
-     success: function(data) {
-         json.forEach(function (value){
-         var img = $("<img />").attr('src', value.['path']);
-         .on('load', function() {
-        	if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-            	alert('broken image!');
-            	$(".four.wide.column").append(img);
-        	} else {
-            $(".four.wide.column").append(img);
-        	}
-    	});
-		});
-	}         
-  });
-}
-
- 
-/*
-function(){
-    $.ajax({ url: '/'
-         , type: 'GET'
-        })
-    .done(function(data) {
-      console.log(data);
-    });
-
-};
-
-
-
-
-}
-
-
-$(document).ready(main);*/
--- server.js
+++ server.js
@@ -15,46 +15,26 @@ var multer= require('multer'),
 var upload=multer({storage:storage});
 var mongodb = require('mongodb');
 
-
-
-
-users=[];
-connections=[];
-images=[];
+var users=[];
+var connections=[];
+var images=[];
 
 
 server.listen(process.env.PORT || 3000);
 console.log('server running...');
 app.use(express.static('./'));
+
 app.get('/', function(req,res){
     res.sendFile(__dirname+'/public/index.html'); 
 });
 
-//mongo DB
-// Connect to the db
- // var MongoClient = mongodb.MongoClient;
- // MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
- //    if(!err) {
- //        console.log("We are connected");
- //    }
- //    else{console.log(err);}
- //    //db.createCollection('images', {strict:true}, function(err, collection) {});        
- //    var collection = db.collection('images');
- //    var stream = collection.find({'id':1}).stream();
- //    stream.on("data", function(item) {
- //        console.log(item);
- //        });
- //    stream.on("end", function() {});
 
- 
-  
     var MongoClient = mongodb.MongoClient;
     MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
     if(!err) {
         console.log("We are connected");
     }
     else{console.log(err);}
-    //db.createCollection('images', {strict:true}, function(err, collection) {});        
     var collection = db.collection('images');
     var stream = collection.find({'id':1}).stream();
     stream.on("data", function(item) {
@@ -72,36 +52,10 @@ app.get('/', function(req,res){
         console.log('No document(s) found with defined "find" criteria!');
       }
   });
- 
 
-
-  
-      // //Close connection
-      // db.close();
    });
-
- 
- // Insert some users
-  // var findfood=function(){
-  //    collection.find({type: 'food'}).toArray(function (err, result) {
-  //     if (err) {
-  //       console.log(err);
-  //     } else if (result.length) {
-  //       console.log('Found:', result);
-  //       return result;
-  //     } else {
-  //       console.log('No document(s) found with defined "find" criteria!');
-  //     }
-  // });
-  
-      // //Close connection
-      // db.close();
-   // };
-
-    
-
-
-io.sockets.on('connection', function(socket){
+  // socketio on  
+  io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('connected: %s sockets connected', connections.length);
 
@@ -124,9 +78,6 @@ io.sockets.on('connection', function(socket){
         updateImage();
 
     });
-
-
-   // io.sockets.emit('images',images);
 
     function updateUsernames(){
         io.sockets.emit('get users',users);
@@ -154,176 +105,6 @@ io.sockets.on('connection', function(socket){
 
 
 
-// app.post('/upload', upload.single('file'),function(req,res,next){
-//     //console.log("data", req.);
-//     var data = req.body;
-//      //upload.any();
-//     console.log(data);//[0]['path']);
-//     //res.send(req.files);
-// });
-
-
-// app.post( "/upload",function(req,res){
-
-//     var upload = multer({ storage : storage}).any(req.file);
-
-//     upload(req,res,function(err) {
-//         if(err) {
-//             console.log(err);
-//             return res.end("Error uploading file.");
-//         }
-//         res.end("File has been uploaded");
-
-//     });
-// });
 
 
 
-
-
-
-
-
-/*
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
-
-
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var multer= require('multer');
-var storage=multer.diskStorage({
-        destination: function(req,file,cb){
-            cb(null, './public/images/');
-        },
-        filename:function (req,file,cb){
-            cb(null,Date.now()+file.originalname);
-        }             
-    });
-var upload=multer({storage:storage});
-
-
-
-
-
-var express=require('express');
-var app = express();
-var server= require('http').createServer(app);
-var sio= require('socket.io');
-
-
-server.listen(process.env.PORT || 3000, function(){
-  console.log('listening on port :3000');
-});
-
-var io=sio.listen(server);
-
-var bodyParser = require('body-parser'),
-    morgan = require('morgan'),
-    multer= require('multer'),
-    storage=multer.diskStorage({
-        destination: function(req,file,cb){
-            cb(null, './public/images/');
-        },
-        filename:function (req,file,cb){
-            cb(null,Date.now()+file.originalname);
-        }             
-    }),
-    upload=multer({storage:storage});
-
-users=[];
-connections=[];
-
-//socket.io
-
-io.sockets.on('connection', function(socket){
-    connections.push(socket);
-    console.log('connected: %s sockets connected', connections.length);
-
-    //discounnect
-    socket.on('disconnect', function(data){
-        //if(! socket.username)return;
-        users.splice(users.indexOf(socket.username),1);
-        connections.splice(connections.indexOf(socket),1);
-        console.log('Disconnected: %s sockets',connections.length) ;
-
-    });
- 
-
-    //new user
-    socket.on('new user',function(data,callback){
-        callback(true);
-        socket.username= data;
-        users.push(socket.username);
-        updateUsernames();
-    });
-
-    function updateUsernames(){
-        io.sockets.emit('get users',users);
-    }
-
-
-
-});
-
-
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
-app.use(express.static('./'));
-
-
-
-// create application/x-www-form-urlencoded parser 
-
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
-
-
-app.get('/', function(req, res) {   
-    // Connect to the db
-   /* MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(!err) {
-        console.log("We are connected");
-    }
-    else{console.log(err);}
-    db.createCollection('images', {strict:true}, function(err, collection) {});    
-    var collection = db.collection('images');
-    var stream = collection.find({'id':1}).stream();
-    stream.on("data", function(item) {
-        //console.log(item);
-        });
-    stream.on("end", function() {});
-
-
-});
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-
-app.post('/',upload.any(),function(req,res,next){
-    console.log(req.files);
-    res.send(req.files);
-});
-
-
-
-
-app.get('/index.html', function(req, res) {
-    res.send('<html><head></head><body>djfhdjskfjdhskfjdjsfjsdjfdsjj</body></html>');
-});
-
-app.get('/hello', function(req, res) {
-    res.send('hello world');
-});
-
-app.get('/goodbye', function(req, res) {
-    res.send('goodbye');
-});
-*/
