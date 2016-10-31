1b2f120f8eeba0bbb5d928d3d180d262de0bf377
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-10-31 14:36:29 -0700

    more data

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: single, undef: true, unused: true, strict: true, trailing: true */
+
+'use strict';
 var express=require('express');
 var app = express();
 var server= require('http').createServer(app);
@@ -15,12 +20,40 @@ var multer= require('multer'),
 var upload=multer({storage:storage});
 var mongodb = require('mongodb');
 
+var MongoClient = mongodb.MongoClient;
+var id;
+
+var db, collection;
+
+var users=[];
+var connections=[];
+var images=[];
+var cfood=[];
+var kfood=[];
+var jfood=[];
+
+MongoClient.connect('mongodb://localhost:27017/exampleDb', function(err, database){
+            if(!err) {
+                console.log('We are connected');
+                db=database;
+                collection = db.collection('images');
+                collection.find({}).toArray(function (err, result) {
+                if (err) {
+                console.log(err);
+                } else if (result.length) {
+                console.log('Found:', result);
+                images= result;
+                } else {
+                console.log('No document(s) found with defined "find" criteria!');
+                }
+                });
+            }
+            else{console.log(err);} 
+});
 
 
 
-users=[];
-connections=[];
-images=[];
+
 
 
 server.listen(process.env.PORT || 3000);
@@ -32,40 +65,109 @@ app.get('/', function(req,res){
 
   
     var MongoClient = mongodb.MongoClient;
-    MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+    MongoClient.connect('mongodb://localhost:27017/exampleDb', function(err, db) {
     if(!err) {
-        console.log("We are connected");
+        console.log('We are connected');
     }
     else{console.log(err);}
    // db.createCollection('images', {strict:true}, function(err, collection) {});    
 
-    var collection = db.collection('images');
-     collection.find({type: 'food'}).toArray(function (err, result) {
-      if (err) {
-        console.log(err);
-      } else if (result.length) {
-        console.log('Found:', result);
-        images= result;
-      } else {
-        console.log('No document(s) found with defined "find" criteria!');
-      }
 
-  });
  
 
 
   
     
    });
+app.get('/allPic',  function(req,res){
+    //res.sendFile(__dirname+'/public/index.html');
 
- 
+     collection.find({}).toArray(function (err, result) {
+                if (err) {
+                console.log(err);
+                } else if (result.length) {
+                console.log('Found:', result);
+                images= result;
+                res.json(images);
+                } else {
+                console.log('No document(s) found with defined "find" criteria!');
+                }
+                });
+});
+   
+
+app.get('/ChinesePic',  function(req,res){
+    //res.sendFile(__dirname+'/public/index.html');
+
+     collection.find({'type':'Chinese'}).toArray(function (err, result) {
+                if (err) {
+                console.log(err);
+                } else if (result.length) {
+                console.log('Found:', result);
+                cfood= result;
+                res.json(cfood);
+                } else {
+                console.log('No document(s) found with defined "find" criteria!');
+                }
+                });    
+});
+   
+app.get('/JapanesePic',  function(req,res){
+    //res.sendFile(__dirname+'/public/index.html');
+
+     collection.find({'type':'Japanese'}).toArray(function (err, result) {
+                if (err) {
+                console.log(err);
+                } else if (result.length) {
+                console.log('Found:', result);
+                jfood= result;
+                res.json(jfood);
+                } else {
+                console.log('No document(s) found with defined "find" criteria!');
+                }
+                });
+});
+
+app.get('/koreanPic',  function(req,res){
+    //res.sendFile(__dirname+'/public/index.html');
+
+     collection.find({'type':'Korean'}).toArray(function (err, result) {
+                if (err) {
+                console.log(err);
+                } else if (result.length) {
+                console.log('Found:', result);
+                kfood= result;
+                res.json(kfood);
+                } else {
+                console.log('No document(s) found with defined "find" criteria!');
+                }
+                });
+});
+
  
 io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('connected: %s sockets connected', connections.length);
 
+    function updateUsernames(){
+        io.sockets.emit('get users',users);
+    }
+
+   function updateImage(){
+    socket.emit('images', images);
+
+    app.post('/', upload.any(), function(req){
+        console.log(req.files);
+        var path = req.files[0].path;
+        console.log('path:', path);
+        io.emit('new image', path);
+       // res.json(req.files);
+    });
+
+
+}
     //discounnect
-    socket.on('disconnect', function(data){
+    socket.on('disconnect', function(){
         //if(! socket.username)return;
         users.splice(users.indexOf(socket.username),1);
         connections.splice(connections.indexOf(socket),1);
@@ -87,23 +189,6 @@ io.sockets.on('connection', function(socket){
 
    // io.sockets.emit('images',images);
 
-    function updateUsernames(){
-        io.sockets.emit('get users',users);
-    }
-
-   function updateImage(){
-    socket.emit('images', images);
-
-    app.post('/', upload.any(), function(req,res,next){
-        console.log(req.files);
-        var path = req.files[0].path;
-        console.log('path:', path);
-        io.emit('new image', path);
-       // res.json(req.files);
-    });
-
-
-}
     
 });
 
